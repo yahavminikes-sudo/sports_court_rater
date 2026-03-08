dd5281c502c47be229bbcf1b07bc5847e7ec3c00
aylongo <aylongo10@gmail.com>
2026-03-08 21:40:30 +0200

    feat(): initialize fragments and set up navigation with view binding

--- app/src/main/java/com/example/sports_court_rater/MainActivity.kt
+++ app/src/main/java/com/example/sports_court_rater/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.example.sports_court_rater
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.core.graphics.toColorInt
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        window.navigationBarColor = Color.WHITE
+        window.navigationBarDividerColor = "#1f000000".toColorInt()
+        val insetsController = WindowInsetsControllerCompat(window, window.decorView)
+        insetsController.isAppearanceLightNavigationBars = true
+
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
