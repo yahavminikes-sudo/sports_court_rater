79534f9b3e238f05dd202ef0c5b26598e240c5fb
aylongo <aylongo10@gmail.com>
2026-03-24 02:10:43 +0200

    fix: added fallback for local db

--- app/src/main/java/com/example/sports_court_rater/di/DatabaseModule.kt
+++ app/src/main/java/com/example/sports_court_rater/di/DatabaseModule.kt
@@ -22,7 +22,9 @@ object DatabaseModule {
             context,
             AppDatabase::class.java,
             "app_database"
-        ).build()
+        )
+        .fallbackToDestructiveMigration()
+        .build()
     }
 
     @Provides
